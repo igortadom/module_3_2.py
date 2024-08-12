b54ca00662dbdbf520d2386696ae23dc11ced748
igortadom <tadom@yandex.ru>
2024-08-12 18:56:25 +0300

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,23 @@
+import re
+
+
+regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+
+def send_email(message, recipient, *, sender='university.help@gmail.com'):
+    ext = ('.com', '.ru', '.net')
+    if re.fullmatch(recipient, sender) and not recipient.endswith(ext) or not sender.endswith(ext):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
+    elif recipient == sender:
+        print('Нельзя отправить письмо самому себе!')
+    elif sender == 'university.help@gmail.com':
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
+    else:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
+
+
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
